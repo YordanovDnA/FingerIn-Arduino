655ff7c062355cf406433806a24cbde17d26bee8
Yordanov <iliyanyyordanov@gmail.com>
2020-05-17 09:41:23 +0100

    Recognising and sending the fingerprint ID to the server

--- FingerIn-Arduino.ino
+++ FingerIn-Arduino.ino
@@ -21,14 +21,27 @@
 
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include "secrets.h"
+#include <Adafruit_Fingerprint.h>
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+// uncomment this line:
+ #define mySerial Serial1
+
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// comment these two lines if using hardware serial
+//SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
 String Name = ""; //TYPE THE NAME OF THE SITE YOU WANT TO CONNECT THE BOARD WITH
-int ID = 120;
-String GET = "GET /add.php?ID="; //GET /add.php?ID=100 HTTP/1.1
-String Send = GET + ID + " HTTP/1.1";
+
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
+char ssid[] = "AndroidAP"; //  your network SSID (name)
+char pass[] = "12345678";    // your network password (use for WPA, or use as key for WEP)
 char server[] = "www.demo.yordanov-web.com";    // name address for Arduino (using DNS)
 
 // Initialize the Wifi client library
@@ -54,7 +67,130 @@ void setup() {
     delay(10000);
   }
   Serial.println("Connected to wifi");
+  
+//Here is coming the fingerprint recognition code
 
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit finger detect test");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  finger.getTemplateCount();
+  Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  Serial.println("Waiting for valid finger...");
+
+//Here the fingerprint recognition code is ending.
+   
+   
+  
+}
+
+
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+  return finger.fingerID;
+
+  
+
+  
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  int ID = finger.fingerID;
+  String GET = "GET /add.php?ID="; //GET /add.php?ID=100 HTTP/1.1
+  String Send = GET + ID + " HTTP/1.1";
+  // found a match!
+  //send fingerprint ID to the web server
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
   if (client.connectSSL(server, 443)) {
@@ -66,9 +202,13 @@ void setup() {
     client.println();
     Serial.println("Request sent");
   }
+  
 }
 
 void loop() {
+  getFingerprintIDez();
+  
+  delay(50);            //don't ned to run this at full speed.
   // if there are incoming bytes available
   // from the server, read them and print them:
   while (client.available()) {
@@ -76,13 +216,5 @@ void loop() {
     Serial.write(c);
   }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
-  }
+  
 }
--- secrets.h/secrets.h.ino
+++
@@ -1,2 +0,0 @@
-char ssid[] = "AndroidAP"; //  your network SSID (name)
-char pass[] = "12345678";    // your network password (use for WPA, or use as key for WEP)
